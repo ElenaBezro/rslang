5f0560ea751f6558e474077c6cda39887deedde0
Elena <lenyka_86@mail.ru>
2022-02-09 01:56:49 +0100

    fix: fix failed test due to use of import.meta

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 require('@testing-library/jest-dom/extend-expect');
+
+// eslint-disable-next-line no-undef
+jest.mock('~/config/env', () => ({
+  SERVER_URL: 'http://localhost:5000',
+}));
