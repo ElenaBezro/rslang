a935101df7525ee92c5b59fbb6e2e2b4a306871f
Elena <lenyka_86@mail.ru>
2022-02-09 01:21:48 +0100

    feat: add scroll to top button

--- src/pages/Dictionary/Dictionary.tsx
+++ src/pages/Dictionary/Dictionary.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 // import { useTranslation } from 'react-i18next';
 import { Grid } from '@mui/material';
 
+import { ScrollToTop } from '~/components/ScrollToTop';
 import { WordCard } from '~/components/WordCard';
 import { PAGES_PER_GROUP_COUNT } from '~/config';
 import { useApi } from '~/hooks';
@@ -21,13 +22,16 @@ const Dictionary = () => {
   }, [group, page, readWords]);
 
   return (
-    <Grid container p={2} spacing={4} overflow="hidden">
-      {(isLoading ? SKELETON_DATA : words?.data || []).map((word, index) => (
-        <Grid key={word?.id ?? index} item xs={12} sm={6} md={4} lg={3}>
-          <WordCard word={word} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container p={2} spacing={4} overflow="hidden">
+        {(isLoading ? SKELETON_DATA : words?.data || []).map((word, index) => (
+          <Grid key={word?.id ?? index} item xs={12} sm={6} md={4} lg={3}>
+            <WordCard word={word} />
+          </Grid>
+        ))}
+      </Grid>
+      <ScrollToTop />
+    </>
   );
 };
 
