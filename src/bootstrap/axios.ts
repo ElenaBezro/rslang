4155ac6fd622e6879131d3c3beeb6d3922354e1b
Elena <lenyka_86@mail.ru>
2022-02-17 01:10:54 +0100

    fix: refresh token

--- src/bootstrap/axios.ts
+++ src/bootstrap/axios.ts
@@ -1,24 +1,50 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import { LOCAL_STORAGE, SERVER_URL } from '~/config';
 
 import { PAGES } from '~/pages';
-import { getLocalStorageValue } from '~/utils/localStorage';
+import { User } from '~/types';
+import { getLocalStorageValue, setLocalStorageValue } from '~/utils/localStorage';
 
 axios.defaults.baseURL = SERVER_URL;
 
-axios.interceptors.request.use((config) => {
-  const token = getLocalStorageValue(LOCAL_STORAGE.TOKEN, null);
-  if (token) {
-    config.headers ||= {};
-    config.headers.Authorization = `Bearer ${token}`;
+const withAuthorization = <T>(config: AxiosRequestConfig<T>, { isRefresh = false, force = false }: { isRefresh?: boolean, force?: boolean } = {}) => {
+  config.headers ||= {};
+  if (force || !config.headers.Authorization) {
+    const token = getLocalStorageValue(isRefresh ? LOCAL_STORAGE.REFRESH_TOKEN : LOCAL_STORAGE.TOKEN, null);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
-});
+};
 
-axios.interceptors.response.use(undefined, (error) => {
-  if (error.status === 401) {
-    // redirect user to home page when not signed in
-    history.replaceState(undefined, '', PAGES.HOME);
+axios.interceptors.request.use((config) => withAuthorization(config));
+
+axios.interceptors.response.use(undefined, (error: AxiosError) => {
+  if (error?.isAxiosError && error.response?.status === 401) {
+    const user = getLocalStorageValue<User | undefined>(LOCAL_STORAGE.USER, undefined);
+    if (user?.id) {
+      const refreshTokenURL = `/users/${user.id}/tokens`;
+      const originalRequest = error.response.config;
+      if (originalRequest.url !== refreshTokenURL) {
+        return axios.get(refreshTokenURL, withAuthorization({}, { isRefresh: true })).then((refreshTokenResponse) => {
+          const { token, refreshToken } = refreshTokenResponse.data ?? {};
+          if (token && refreshToken) {
+            setLocalStorageValue(LOCAL_STORAGE.TOKEN, token);
+            setLocalStorageValue(LOCAL_STORAGE.REFRESH_TOKEN, refreshToken);
+            // retry original request with a new token
+            return axios(withAuthorization(originalRequest, { force: true }));
+          }
+
+          history.replaceState(undefined, '', PAGES.LOGOFF);
+          return Promise.reject(error);
+        });
+      }
+    }
+
+    // redirect user to logoff page
+    history.replaceState(undefined, '', PAGES.LOGOFF);
   }
-  throw error;
+
+  return Promise.reject(error);
 });
